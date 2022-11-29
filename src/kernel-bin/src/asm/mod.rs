40f8335887f87bb98831310c0945abf4e464e218
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2022-11-29 18:04:17 +0100

    drop EFI system state, use i386 system state

--- src/kernel-bin/src/asm/mod.rs
+++ src/kernel-bin/src/asm/mod.rs
@@ -10,8 +10,17 @@
 // code to ".section .foo", the symbols will be compiled and linked in the same order into the
 // binary.
 
+core::arch::global_asm!(include_str!("known_registers.S"), options(att_syntax));
+// macros for start.S and other files
 core::arch::global_asm!(include_str!("macros.S"), options(att_syntax));
+// main file
 core::arch::global_asm!(include_str!("start.S"), options(att_syntax));
+// Helper functions used in start.S. Useful for debugging.
 core::arch::global_asm!(include_str!("debugcon.S"), options(att_syntax));
+// Strings for printing. Useful for debugging.
 core::arch::global_asm!(include_str!("strings.S"), options(att_syntax));
+// Static backing memory.
+core::arch::global_asm!(include_str!("static_memory.S"), options(att_syntax));
+
+// Multiboot2 header
 core::arch::global_asm!(include_str!("mb2_header.S"));
