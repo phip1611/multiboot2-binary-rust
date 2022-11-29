da68cb106867ade8dd849b50ea75202b1aaa3184
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2022-11-29 11:57:17 +0100

    start.S print to QEMUs debugcon device

--- src/kernel-bin/src/asm/mod.rs
+++ src/kernel-bin/src/asm/mod.rs
@@ -6,6 +6,12 @@
 //! clear whether something is an immediate or an address. Additionally, there is more existing
 //! assembly code available in AT&T syntax for comparison.
 
+// To the compiler, this will look like a big single assembly file. Hence, if multiple files add
+// code to ".section .foo", the symbols will be compiled and linked in the same order into the
+// binary.
+
 core::arch::global_asm!(include_str!("macros.S"), options(att_syntax));
 core::arch::global_asm!(include_str!("start.S"), options(att_syntax));
+core::arch::global_asm!(include_str!("debugcon.S"), options(att_syntax));
+core::arch::global_asm!(include_str!("strings.S"), options(att_syntax));
 core::arch::global_asm!(include_str!("mb2_header.S"));
